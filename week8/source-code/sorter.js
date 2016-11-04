dbecbb565579c9accfb93925772a4303755da7c4
liaozelin <489531912@qq.com>
2016-11-04 22:51:27 +0800

    week8

--- week8/source-code/sorter.js
+++ week8/source-code/sorter.js
@@ -1,23 +1,23 @@
 //http://www.cplusplus.com/reference/string/string/?kw=string
 (function() {
-    jq = document.createElement('script');
+    var jq = document.createElement('script');
     jq.type = "text/javascript";
     jq.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.js";
-    lodash = document.createElement('script');
+    var lodash = document.createElement('script');
     lodash.type = "text/javascript";
     lodash.src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js";
     $('head').append(jq);
     $('head').append(lodash);
     var run = function() {
-        content = $('table').children();
+        var content = $('table').children();
         $(content).each(function() {
-            thead = $(this).children()[0];
-            tdata = $(this).children();
+            var thead = $(this).children()[0];
+            var tdata = $(this).children();
             tdata.splice(0, 1);
-            sorted1 = _.sortBy(tdata, (o) => $($(o).children()[0]).text());
-            sorted2 = _.sortBy(tdata, (o) => $($(o).children()[1]).text());
-            count1 = 0;
-            count2 = 0;
+            var sorted1 = _.sortBy(tdata, (o) => $($(o).children()[0]).text());
+            var sorted2 = _.sortBy(tdata, (o) => $($(o).children()[1]).text());
+            var count1 = 0;
+            var count2 = 0;
             $(thead.children[0]).click(function() {
                 if (++count1 % 2 === 1) $(this).parent().parent().append(sorted1);
                 else $(this).parent().parent().append(sorted1.reverse());
